436dca5c22ff10629befbbae187058d13d294dc3
Leo Hong <jhongan@ust.hk>
2023-12-13 09:50:00 +0800

    Delete STM32 directory

--- STM32/Core/Inc/hmc.h
+++
@@ -1,5 +0,0 @@
-#include "stm32f1xx_hal.h"
-
-    void HMC5883L_Init(I2C_HandleTypeDef *hi2c2);
-    int HMC5883L_Read(I2C_HandleTypeDef *hi2c2);
-
--- STM32/Core/Inc/led.h
+++
@@ -1,8 +0,0 @@
-#ifndef LED_H
-#define LED_H
-#include "main.h"
-
-int rgb_state = 0;
-uint16_t gpioPin[] = {GPIO_PIN_1, GPIO_PIN_5, GPIO_PIN_0};
-
-#endif /* LED_H */
